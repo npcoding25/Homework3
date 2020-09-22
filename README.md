1f57af873bcd913bf7b1f470d3a58924f1d90081
Nathan Polomark <polomarkn@hotmail.com>
2020-09-22 13:37:22 -0700

    Organized lines, added comments, completed readMe

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Homework3
+# Password Generator
+
+## Objective
+
+* To create a website that gives you a randomly generated password based on the criteria you ask for.
+
+## Functionality
+
+When you go on the webpage you will be asked a series of questions.
+  * How many characters do you want?
+  * Do you want special characters?
+  * Do you want numbers?
+  * Do you want uppercase letters?
+  * Do you want lowercase letters?
+
+After choosing your selected characters you can click on the 'Generate Password' button. This will give you a password with all your selected criteria.
+
+### Challenges
+
+There were many obstacles on the way to make the password generator work. I over thought the functions and I was making it way more work then it needed to be. I wanted the questions to come up after i pressed the button so I tried putting everything inside the 'writePassword()' but i kept getting errors and could not get it to work.
+
+### Screenshot
+
+![image](https://user-images.githubusercontent.com/69565347/93932379-ef85a180-fcd4-11ea-9368-78c259216d01.png)
+
+Click [Here](https://npcoding25.github.io/Homework3.Password-Generator/) to see launched website.
--- script.js
+++ script.js
@@ -1,34 +1,31 @@
+// List of variables
 var passwordLength = prompt("How many characters do you want?")
 var passwordSpecialChar = confirm("Do you want special characters")
 var passwordNumber = confirm("Do you want numbers?")
 var passwordUpperCase = confirm("Do you want uppercase letters?")
 var passwordLowerCase = confirm("Do you want lowercase letters?")
+
+// Password that user will see on screen
 var finalPassword = ""
 
-
+// Choosing characters from the answers of the previous variables
 var finalCharSet = ""
-if(passwordSpecialChar) finalCharSet += "!@#$%^&*()"
-if(passwordNumber) finalCharSet += "0123456789"
-if(passwordUpperCase) finalCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-if(passwordLowerCase) finalCharSet += "abcdefghijklmnopqrstuvwxyz"
+  if(passwordSpecialChar) finalCharSet += "!@#$%^&*()"
+  if(passwordNumber) finalCharSet += "0123456789"
+  if(passwordUpperCase) finalCharSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  if(passwordLowerCase) finalCharSet += "abcdefghijklmnopqrstuvwxyz"
 
+// Loop to take the chosen password length and the selected characters and set it as the password
 for( var i = 0; i < passwordLength; i++) {
   var selectedChar = parseInt( Math.random()*finalCharSet.length )
-   finalPassword += finalCharSet[selectedChar]
-  
+  finalPassword += finalCharSet[selectedChar]
 }
 
-
+// Function that applies the new password to textarea
 function writePassword() {
-   document.querySelector("#finalPassword").value = finalPassword
+  document.querySelector("#password").value = finalPassword
+} 
 
-  // var finalPasswordText = document.querySelector("#password");
-  
-  // finalPasswordText.display = finalPassword;
-}
- 
-  var generateBtn = document.querySelector("#generate");
-  
-  
-// Add event listener to generate button
+// Making the button work and apply the function
+var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword); 
